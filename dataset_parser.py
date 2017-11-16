258b7c2e4d085bd567744078522ac72f041fcd8f
ilan56 <ilan56>
2017-11-16 17:36:07 -0500

    First commit, dataset parser hardcoded for hip-hop

---
+++ dataset_parser.py
@@ -0,0 +1,32 @@
+import csv
+import collections
+
+hiphop = []
+genre = []
+dates = []
+songs_per_year = {}
+
+with open('lyrics.csv', 'rb') as csv_lyrics:
+	csv_reader = csv.reader(csv_lyrics, delimiter=',')
+
+	for row in csv_reader:
+		curr_genre = row[4]
+		song = row[1]
+		curr_date = row[2]
+
+		if curr_genre == "Hip-Hop":
+			hiphop.append(song)
+			if curr_date not in dates:
+				dates.append(curr_date)
+			if curr_date in songs_per_year:
+				songs_per_year[curr_date] += 1
+			else:
+				songs_per_year[curr_date] = 1
+
+
+print(len(hiphop))
+dates.sort(key=int)
+print(dates)
+songs_per_year = collections.OrderedDict(sorted(songs_per_year.items()))
+for year in songs_per_year:
+	print(str(year) + ": " + str(songs_per_year[year]))
