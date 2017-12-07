3f73cb801d72938af2c44059f4690237e6ca7467
gherman27 <gregoryherman@1x-nat-vl931-172-30-183-91.wireless.umass.edu>
2017-12-07 14:53:52 -0500

    Calculates emotion vector for songs and years

--- polarity.py
+++ polarity.py
@@ -4,7 +4,9 @@ import math
 import collections
 import csv
 from string import punctuation
+import matplotlib.pyplot as plt
 
+emotions = ["angry", "disgust", "happy", "horror", "sad", "surprise"]
 year_span = ["2006", "2007", "2008", "2009", "2010", "2011", "2012", "2013", "2014", "2015", "2016"]
 year_songs = {x : [] for x in year_span}
 
@@ -12,6 +14,10 @@ max_num_songs = 1500
 
 year_song_count = {x : 0 for x in year_span}
 year_lyric_count = {x : {} for x in year_span}
+
+year_emotion_lyric_count = {y : {e : {} for e in emotions} for y in year_span}
+year_emotion_count = {y : {e : 1 for e in emotions} for y in year_span}
+
 year_lyric_count_angry = {x : {} for x in year_span}
 year_lyric_count_disgust = {x : {} for x in year_span}
 year_lyric_count_happy = {x : {} for x in year_span}
@@ -26,6 +32,12 @@ horror_count = 1
 sad_count = 1
 surprise_count = 1
 
+seeds = {"angry" : ["angry", "ugly", "mad", "anxious", "jaded", "ignorant", "frustrated", "jealous", "emotional", "insecure", "hungry", "confused", "cynical"], 
+         "disgust" : ["disgust","scorn", "contempt", "disdain", "hatred", "guilt", "bitterness", "apathy", "anxiety", "fury", "vile", "anguish"], 
+         "happy" : ["happy", "glad", "lucky", "alive", "good", "pleased", "satisfied", "thankful", "fine", "complete", "grateful", "content", "perfect"], 
+         "horror" : ["horror", "chaos", "terror", "delusion", "gloom", "plague", "violence", "blackness", "bloodshed", "cruelty", "madness"],
+         "sad" :  ["sad", "lonely", "miserable", "depressed", "boring", "tragic", "pathetic", "hopeless", "weird", "helpless"], 
+         "surprise" : ["surprise", "warning", "shame", "mistake", "fool", "joke", "mystery", "thrill", "gift", "coincidence", "chance"]}
 
 angry_seeds = ["angry", "ugly", "mad", "anxious", "jaded", "ignorant", "frustrated", "jealous", "emotional", "insecure", "hungry", "confused", "cynical"]
 disgust_seeds = ["disgust","scorn", "contempt", "disdain", "hatred", "guilt", "bitterness", "apathy", "anxiety", "fury", "vile", "anguish"]
@@ -56,8 +68,13 @@ with open('lyrics.csv', 'rb') as csv_lyrics:
             lyrics = lyrics.translate(None, punctuation)
             lyric_list = lyrics.split(" ")
             
-            
-            
+            is_emot = {e : False for e in emotions}
+            for emot in emotions: 
+                 if any(x in lyric_list for x in seeds[emot]):
+                        is_emot[emot] = True
+                        year_emotion_count[curr_year][emot] += 1
+           
+            """            
             is_angry = False
             if any(x in lyric_list for x in angry_seeds):
                 angry_count += 1
@@ -82,8 +99,10 @@ with open('lyrics.csv', 'rb') as csv_lyrics:
             if any(x in lyric_list for x in surprise_seeds):
                 surprise_count += 1
                 is_surprise = True
+            """
             
             for lyric in lyric_list:
+                
                 if lyric == punctuation:
                     lyric_list.remove(lyric)
                     continue
@@ -92,8 +111,10 @@ with open('lyrics.csv', 'rb') as csv_lyrics:
                     continue
                     
                 if lyric not in year_lyric_count[curr_year]:
-                    year_lyric_count[curr_year][lyric] = 1
-                    year_lyric_count_angry[curr_year][lyric] = 1
+                    year_lyric_count[curr_year][lyric] = 1 
+                    for emot in emotions: 
+                        year_emotion_lyric_count[curr_year][emot][lyric] = 1
+                    #year_lyric_count_angry[curr_year][lyric] = 1
                     #year_lyric_count_disgust[curr_year][lyric] = 1
                     #year_lyric_count_happy[curr_year][lyric] = 1
                     #year_lyric_count_horror[curr_year][lyric] = 1
@@ -102,8 +123,14 @@ with open('lyrics.csv', 'rb') as csv_lyrics:
                     
                 else:
                     year_lyric_count[curr_year][lyric] += 1
-                if is_angry:
-                    year_lyric_count_angry[curr_year][lyric] += 1
+                
+                for emot in emotions: 
+                    if is_emot[emot]:
+                        year_emotion_lyric_count[curr_year][emot][lyric] += 1 
+                
+                
+            #    if is_angry:
+            #        year_lyric_count_angry[curr_year][lyric] += 1
             #   if is_disgust:
             #       year_lyric_count_disgust[curr_year][lyric] += 1
             #	if is_happy:
@@ -116,27 +143,46 @@ with open('lyrics.csv', 'rb') as csv_lyrics:
             #       year_lyric_count_surprise[curr_year][lyric] += 1
             year_songs[curr_year].append(lyric_list)
 
+year_emotion_PMI_lyric = {y : {e : {} for e in emotions} for y in year_span}
+year_emotion_PMI_song = {y : {e : [] for e in emotions} for y in year_span}
+            
 year_PMI_lyric_angry = {x : {} for x in year_span}
 year_PMI_song_angry = {x : [] for x in year_span}
             
 for year in year_span:
-    for lyric, var in year_lyric_count[year].items():
-        co_occ = year_lyric_count_angry[year][lyric] / max_num_songs
-        occ_x = year_lyric_count[year][lyric] / max_num_songs
-        occ_y = angry_count / max_num_songs
-        year_PMI_lyric_angry[year][lyric] = co_occ / (occ_x * occ_y)
-        
-    for lyric_list in year_songs[year]:
-        PMI_sum_angry = 0
-        for lyric in lyric_list: 
-            PMI_sum_angry += year_PMI_lyric_angry[year][lyric]
-        year_PMI_song_angry[year].append(PMI_sum_angry)
+    for emot in emotions:  
+        for lyric, var in year_lyric_count[year].items():     
+            co_occ = year_emotion_lyric_count[year][emot][lyric] / max_num_songs
+            occ_x = year_lyric_count[year][lyric] / max_num_songs
+            occ_y = year_emotion_count[year][emot] / max_num_songs
+            year_emotion_PMI_lyric[year][emot][lyric] = math.log(co_occ / (occ_x * occ_y))
+    
+        for lyric_list in year_songs[year]:
+            PMI_sum_emot = 0
+            for lyric in lyric_list: 
+                if lyric not in year_emotion_PMI_lyric[year][emot]: 
+                    continue
+                PMI_sum_emot += year_emotion_PMI_lyric[year][emot][lyric]
+            year_emotion_PMI_song[year][emot].append(PMI_sum_emot)
 
-year_Anger_score = {x : sum(year_PMI_song_angry[x]) / len(year_PMI_song_angry[x]) for x in year_span}
-print (year_Anger_score)
+year_emot_score = {y : {e : sum(year_emotion_PMI_song[y][e]) / len(year_emotion_PMI_song[y][e]) for e in emotions} for y in year_span}
             
-            
-            
+#year_Anger_score = {x : sum(year_PMI_song_angry[x]) / len(year_PMI_song_angry[x]) for x in year_span}
+
+print (year_emot_score)
+   
+"""    
+pts = []    
+for year in year_span:
+    pts.append(year_Anger_score[year])
+print(pts)
+
+plt.plot(pts, "r-")
+plt.axis([2006, 2016, min(pts) - 10, max(pts) + 10])
+ax = plt.gca()
+ax.set_autoscale_on(False)
+plt.show()
+"""
             
             
 """
